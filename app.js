42bbd1416b8d41c2ed5b3e613b54fc95a645c970
Soheil Najmabadi Kia <sohnajm@gmail.com>
2022-11-25 11:39:27 -0330

    objects

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 function adding(x,y) {
     console.log("adding: ", x + y);
-    //return x + y;
+
+    const person = {
+  firstName: "John",
+  lastName : "Doe",
+  id       : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+    document.getElementById("demo2").innerHTML = person.fullName();
+    
+    return x + y;
 }
 
-adding(4, 3);
-adding(4, 5);
+adding(5, 6);
+
+document.getElementById("demo3").innerHTML = adding(1, 6);
+
+
