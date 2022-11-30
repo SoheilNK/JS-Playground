d5f377e0f704d78ccba8542ca0ac994fcab2c7fb
Soheil Najmabadi Kia <sohnajm@gmail.com>
2022-11-29 22:37:20 -0330

    timer2

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-function adding(x,y) {
-    console.log("adding: ", x + y);
+var x;
 
-    const person = {
-  firstName: "John",
-  lastName : "Doe",
-  id       : 5566,
-  fullName : function() {
-    return this.firstName + " " + this.lastName;
-  }
-};
+function cdTimer() {
+  // Set the time in minutes
+  let t1 = document.getElementById("cdTime").value;
 
-    document.getElementById("demo2").innerHTML = person.fullName();
-    
-    return x + y;
+  var oldDate = new Date();
+  var countDownDate = new Date(oldDate.getTime() + t1 * 60000 + 2000);
+  console.log("goto setInterval");
+  var x = setInterval(count, 1000);
+  
 }
 
-adding(5, 6);
 
-document.getElementById("demo3").innerHTML = adding(1, 6);
+function count() {
+      // Get today's date and time
+      let now = new Date().getTime();
+    console.log(now);
+      // Find the distance between now and the count down date
+      let distance = countDownDate - now;
+      let minutes = Math.floor(distance/60000);
+      let secondes = Math.floor((distance % 60000) / 1000)
+      document.getElementById("timer").innerHTML = minutes + " : " + secondes;
+      
+      if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("timer").innerHTML = "EXPIRED";
+      }
+  }
+function cdTimer2() {
+  // Set the time in minutes
+  let t1 = document.getElementById("cdTime").value;
 
+  var oldDate = new Date();
+  var countDownDate = new Date(oldDate.getTime() + t1 * 60000 + 2000);
+  console.log("goto setInterval");
+  
+  do {
+  // Get today's date and time
+  var now = new Date().getTime();
 
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  var minutes = Math.floor(distance/60000);
+  var secondes = Math.floor((distance % 60000) / 1000)
+  document.getElementById("timer").innerHTML = minutes + " : " + secondes;
+}
+while (distance > 0);
+  
+}
